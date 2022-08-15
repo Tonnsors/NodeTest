144113012bdf29c933e1ddaab74a79fc5b8b960b
Tonnsors <42954982+Tonnsors@users.noreply.github.com>
2022-08-15 03:29:39 -0500

    Add files via upload

--- src/dbconnections.js
+++ src/dbconnections.js
@@ -4,7 +4,6 @@ var Db = require("mysql");
 let connection = Db.createConnection(config);
 async function getLibros(){
     let data;
-    //data = connection.query("INSERT INTO libros2 (idlibros2,titulo,autor,publicacion,editorial) values (6,'libro6','juan','marzo2004','publicadora2')");
     data=connection.query("SELECT * FROM libros2");
     return data;
 }
